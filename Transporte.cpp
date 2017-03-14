4246c8b4ca910e1b42fdff19051f4fdf696d187c
Sanchez10 <joao.sanchez01@gmail.com>
2017-03-14 11:17:21 -0300

    Add files via upload

---
+++ Transporte.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int A, B, C, X, Y, Z;
+
+int main (void)
+{
+	cin >> A;
+	cin >> B;
+	cin >> C;
+	cin >> X;
+	cin >> Y;
+	cin >> Z;
+	cout << ((X/A)*(Y/B)*(Z/C));
+	return 0;
+}
